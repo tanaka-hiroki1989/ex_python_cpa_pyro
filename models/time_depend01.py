6f7518d3aac55e65c45fdab3d295a977df683387
Hiroki Tanaka <rokitaniplus1@gmail.com>
2022-12-13 00:22:03 +0900

    modified time_depend01

--- models/time_depend01.py
+++ models/time_depend01.py
@@ -6,9 +6,9 @@ from customizedpyro.distributions.poibin import PoissonBinomial
 # from customizedtorch.constrains import zero_to_one
 
 class model2:
-    def __init__(self,data,hp_a1,hp_b1,hp_a2,hp_b2,init_a1,init_b1,init_a2,init_b2):
+    def __init__(self,data,N,hp_a1,hp_b1,hp_a2,hp_b2,init_a1,init_b1,init_a2,init_b2):        
         self.data=data
-        self.N=self.data.size(0)
+        self.N=N
         self.hp_a1=hp_a1
         self.hp_b1=hp_b1
         self.hp_a2=hp_a2 
@@ -17,26 +17,19 @@ class model2:
         self.init_b1=init_b1
         self.init_a2=init_a2 
         self.init_b2=init_b2
-    def generator(self):   
-        """
-        a1 = 1.0
-        b1 = 1.0
-        a2 = 1.0
-        b2 = 1.0
-        """
+
+    def generator(self,data):   
         lambda1 = pyro.sample("lambda1", Gamma(self.hp_a1,self.hp_b1))
         lambda2 = pyro.sample("lambda2", Gamma(self.hp_a2,self.hp_b2))
-        p = torch.tensor([0.0]*(N-1),dtype=torch.double)
+        p = torch.tensor([0.0]*(self.N-1),dtype=torch.double)
         tau = pyro.sample("tau", PoissonBinomial(torch.sigmoid(p)))
         z_seq_before = torch.tensor([lambda1 * i for i in range(tau+1)])
-        z_seq_after = torch.tensor([lambda1 * (tau-1) +lambda2 * (i-tau) for i in range(tau+1,N)])
+        z_seq_after = torch.tensor([lambda1 * (tau-1) +lambda2 * (i-tau) for i in range(tau+1,self.N)])
         z_seq = torch.concat([z_seq_before,z_seq_after])
+        with pyro.plate("data", self.N):
+            pyro.sample("obs", Normal(z_seq, torch.tensor([1.0]*self.N)), obs=self.data)
 
-        with pyro.plate("data", N):
-            pyro.sample("obs", Normal(z_seq, torch.tensor([1.0]*N)), obs=self.data)
-
-    def inference(self):
-        #N=100
+    def inference(self,data):
         a1 = pyro.param('a1', lambda: torch.tensor(self.init_a1,dtype=torch.double),constraint=positive)
         b1 = pyro.param('b1', lambda: torch.tensor(self.init_b1,dtype=torch.double),constraint=positive)
         a2 = pyro.param('a2', lambda: torch.tensor(self.init_a2,dtype=torch.double),constraint=positive)
--- utils/plot_gamma.py
+++ utils/plot_gamma.py
@@ -6,7 +6,7 @@ def plot_gamma(step,a1,b1,a2,b2):
     x = np.linspace(max(gamma.ppf(0.0,a1,b1),gamma.ppf(0.0,a2,b2)),
                     max(gamma.ppf(0.999,a1,b1),gamma.ppf(0.999,a2,b2)),100)
     ax.set_xlim(0.0,20.0)
-    ax.set_ylim(0.0,0.3)
+    ax.set_ylim(0.0,1.0)
     ax.plot(x, gamma.pdf(x,a1, b1))
     ax.plot(x, gamma.pdf(x,a2, b2))
-    fig.savefig("result02/"+str(step)+".png")
+    fig.savefig("results/results02/"+str(step)+".png")
